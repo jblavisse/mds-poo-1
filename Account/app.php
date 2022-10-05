ada8591d1b626b098da03538e2adad0492427ddb
JB <jb@levillageducode.fr>
2022-10-05 16:44:30 +0200

    Add takeCash and calcInterest methods

--- Account/app.php
+++ Account/app.php
@@ -1,26 +1,85 @@
 <?php
 
 class Account {
-    private $balance;
+    protected $balance;
 
-    public function __construct() {
-        $this->balance = 0;
+    public function __construct($balance) {
+        $this->setBalance($balance);
     }
-}
 
-
-class SavingAccount extends Account {
     function getBalance() {
         echo $this->balance;
     }
+
+    function setBalance($balance) {
+        $this->balance = $balance;
+    }
 }
 
+
+// Compte Epargne
+class SavingAccount extends Account {
+    protected $interestRate;
+
+    // Ajouter un constructeur
+    public function __construct($balance, $rate) {
+        $this->setBalance($balance);
+        $this->setInterestRate($rate);
+    }
+
+    public function getInterestRate() {
+        return $this->interestRate;
+    }
+
+    public function setInterestRate($rate) {
+        $this->interestRate = $rate;
+    }
+
+    // calcInterest()
+    public function calcInterest() {
+        // solde x taux d'intérêt/100
+        $calc = $this->balance * $this->interestRate / 100;
+        echo $calc." euros d'intérêt".PHP_EOL;
+    }
+}
+
+
+// Compte Payant
 class PayingAccount extends Account {
+    protected $cost;
 
+    public function __construct($balance, $cost) {
+        $this->setBalance($balance);
+        $this->setCost($cost);
+    }
+
+    public function getCost() {
+        return $this->cost;
+    }
+
+    public function setCost($cost) {
+        $this->cost = $cost;
+    }
+
+    public function takeCash($cash) {
+
+        // Je veux retirer 200 euros
+
+        // Si mon solde est à 700 euros
+
+        // Le cout de chaque retrait, c'est 10
+
+        // 700 - 200 - 10
+        $newBalance = $this->balance - $cash - $this->cost;
+        echo "Le nouveau solde, c'est ".$newBalance." euros";
+        $this->setBalance($newBalance);
+    }
 }
 
-$account1 = new SavingAccount();
-$account2 = new PayingAccount();
+// $account1 = new SavingAccount(300, 6);
+// $account1->calcInterest();
 
-$account1->getBalance();
-print($account1->balance);
+
+$account2 = new PayingAccount(700, 10);
+$account2->takeCash(200);
+echo $account2->getBalance();
--- Animal/app.php
+++ Animal/app.php
@@ -3,19 +3,31 @@
 class Animal {
     protected $nbPaws;
 
-    public function __construct() {
-        $this->nbPaws = 4;
+    public function __construct($nbPaws) {
+        $this->nbPaws = $nbPaws;
     }
 
     public function getNbPaws() {
         echo "Nb de patounes: ".$this->nbPaws.PHP_EOL;
     }
+
+    public function setNbPaws($nbPaws) {
+        $this->nbPaws = $nbPaws;
+    }
 }
+
+$dumbo = new Animal(7);
+var_dump($dumbo);
 
 
 class Cat extends Animal {
     // Elle n'est pas accessible dans la classe mère
     private $jumpHeight;
+
+    public function __construct($nbPaws,$jumpHeight) {
+        $this->setNbPaws($nbPaws);
+        $this->setJumpHeight($jumpHeight);
+    }
 
     public function setJumpHeight($jump) {
         $this->jumpHeight = $jump;
@@ -27,8 +39,7 @@ class Cat extends Animal {
 
 }
 
-$garfield = new Cat();
-var_dump($garfield);
+$garfield = new Cat(5,3);
+
 $garfield->getNbPaws();
-$garfield->setJumpHeight(3);
 echo $garfield->getJumpHeight();
