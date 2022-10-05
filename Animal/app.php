3b9377fed7b9b2be6d0ce4882599c62b3f80c3d1
JB <jb@levillageducode.fr>
2022-10-05 15:18:41 +0200

    Explain protected visibility for Animal

--- Animal/app.php
+++ Animal/app.php
@@ -1,7 +1,7 @@
 <?php
 
 class Animal {
-    public $nbPaws;
+    protected $nbPaws;
 
     public function __construct() {
         $this->nbPaws = 4;
@@ -14,9 +14,21 @@ class Animal {
 
 
 class Cat extends Animal {
+    // Elle n'est pas accessible dans la classe mère
+    private $jumpHeight;
+
+    public function setJumpHeight($jump) {
+        $this->jumpHeight = $jump;
+    }
+
+    public function getJumpHeight() {
+       return $this->jumpHeight;
+    }
 
 }
 
 $garfield = new Cat();
 var_dump($garfield);
 $garfield->getNbPaws();
+$garfield->setJumpHeight(3);
+echo $garfield->getJumpHeight();
